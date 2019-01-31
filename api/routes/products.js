88692a82a86a3ffe66d7681dbb29d5dc9ca16005
rjdeus <41559152+rjdeus@users.noreply.github.com>
2019-01-31 00:01:38 -0500

    added routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,13 +3,39 @@ const router = express.Router();
 
 router.get('/',(req,res,next)=>{
     res.status(200).json({
-        message:'get req'
+        message:'Handling GET requests to /products'
     })
 })
 
 router.post('/',(req,res,next)=>{
+    res.status(201).json({
+        message:'Handling POST requests to /products'
+    })
+})
+
+router.get('/:productId',(req,res,next)=>{
+    const id = req.params.productId;
+    if (id == 'special'){
+        res.status(200).json({
+            message: 'you discovered the special ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'you passed an ID'
+        })
+    }
+})
+
+router.patch('/:productId',(req,res,next)=>{
     res.status(200).json({
-        message:'abc'
+        message: 'updated product'
+    })
+})
+
+router.delete('/:productId',(req,res,next)=>{
+    res.status(200).json({
+        message: 'delete product'
     })
 })
 
